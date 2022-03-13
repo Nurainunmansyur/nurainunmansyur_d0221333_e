a3da41e9f2a44e00b36aa2e91c67203917fb5a96
Nurainumansyur <ainunsyurda01@gmail.com>
2022-03-13 23:24:14 +0700

    algoritma dan struktur data

---
+++ jenis jenis tipe data python.py
@@ -0,0 +1,47 @@
+#tipe data python
+#tipe data boolean
+foo = True
+boo = False
+print(foo)
+print(boo)
+
+foo = 12 < 10
+print(foo)
+foo = 12 > 10
+print(foo)
+foo = "A" == "a"
+print(foo)
+
+#tipe data string
+print("ayo belajar python")
+print('Belajar Python Sangat Menyenangkan')
+
+#tipe data integer
+banyak_buah = 10
+print("banyak buah : ", banyak_buah)
+
+banyak_mobil = 5
+print(banyak_mobil)
+
+#tipe data float
+alas_segitiga = 5.5
+print("alas segitiga : ", alas_segitiga)
+tinggi = 7.5
+print("tinggi : ", tinggi)
+
+#tipe data list
+buah = (["apel", "anggur", "rambutan", "durian", "langsat"])
+print([buah])
+
+#tipe data tuple
+angka = (("1", "2", "3", "4", "5"))
+print((angka))
+
+#tipe data dictionary
+print("nama : ainun ", "umur : 18 ")
+
+#tipe data complex
+print(5j)
+
+#tipe data hexadecimal
+print(9)
