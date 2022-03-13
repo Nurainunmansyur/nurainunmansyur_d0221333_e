97ce6c9f767bcb3c2416ac8239a8b2f866bcfafc
Nuraiunmansyur <ainunsyurda@gmail.com>
2022-03-13 23:34:34 +0700

    algoritma dan struktur data

---
+++ operator dalam python.py
@@ -0,0 +1,86 @@
+#operator
+#operator aritmatika
+bilangan1 = 10
+bilangan2 = 5
+#penjumlahan
+print(bilangan1 + bilangan2)
+#pengurangan
+print(bilangan1 - bilangan2)
+#perkalian
+print(bilangan1 * bilangan2)
+#pembagian
+print(bilangan1 / bilangan2)
+#sisa bagi
+print(bilangan1 % bilangan2)
+#pangkat
+print(bilangan1 ** bilangan2)
+#pembagian bilangan bulat
+print(bilangan1 // bilangan2)
+
+#operator penugasan
+angka1 = 7
+angka2 = 3
+#tambah sama dengan
+angka1 += 2
+print(angka1)
+#kurang sama dengan
+angka1 -= 2
+print(angka1)
+#kali sama dengan
+angka1 *= 2
+print(angka1)
+#bagi sama dengan
+angka1 /= 2
+print(angka1)
+#sisa bagi sama dengan
+angka1 %= 2
+print(angka1)
+#pangkat sama dengan
+angka1 **= 2
+print(angka1)
+#pembagian bilangan bulat sama dengan
+angka1 //= 2
+print(angka1)
+
+#operator perbandingan
+nilai1 = 5
+nilai2 = 3
+#sama dengan
+nilai1 == 2
+print(nilai1)
+nilai2 == 2
+print(nilai2)
+#tidak sama dengan
+nilai1 != 2
+print(nilai1)
+#lebih kecil dari
+nilai1 < 2
+print(nilai1)
+#lebih besar dari
+nilai1 > 2
+print(nilai1)
+#lebih kecil sama dengan
+nilai1 <= 2
+print(nilai1)
+#lebih besar sama dengan
+nilai1 >= 2
+print(nilai1)
+
+#operator logika
+#logika and
+print('hasil dari True and True : ', True and True)
+print('hasil dari True and False : ', True and False)
+print('hasil dari False and True : ', False and True)
+print('hasil dari False and False : ', False and False)
+
+#logika or
+print('hasil dari True or True : ', True or True)
+print('hasil dari True or False : ', True or False)
+print('hasil dari False or True : ', False or True)
+print('hasil dari False or True : ', False or False)
+
+#negasi atau kebalikan (not)
+print('hasil dari not True : ', not True)
+print('hasil dari not False : ', not False)
+
+
